69562ad8eba3e9ac1a663dc5bd099d6b29882ccc
tosiab <mtabednarz@gmail.com>
2024-10-15 14:34:30 -0400

    Initial commit

---
+++ TextAdventure/TextAdventure.java
@@ -0,0 +1,129 @@
+import java.util.*;
+
+public class TextAdventure 
+{
+  FancyConsole console;
+  Scanner inScanner;
+  Player ourHero;
+
+  public TextAdventure()
+  {
+    console = new FancyConsole("Great Text Adventure!", 600, 600);
+    inScanner = new Scanner(System.in);
+
+    // feel free to change the player's starting values
+    ourHero = new Player("Bob", 100, 0);
+  }
+
+  public void play()
+  {
+    String input;
+    // start of adventure. You can change this if you like
+    console.setImage("distantcity.jpg");
+
+    // ask the user for their name.
+    System.out.println("What is your name?\n");
+    input = inScanner.nextLine();
+
+    // Change ourHero's name
+    // ADD CODE HERE
+    
+    // describe the starting situation. Feel free to change this
+    System.out.println("You wake up to find yourself on the edge of a shadowy forest with the sun nearly set. \nYou see what looks like a city in the distance. \nWhat would you like to do? \ncity: go towards the city\nforest: turn around and re-enter the forest\nnap: go back to sleep\n" + ourHero.getName() + ": ");
+
+    // get user input and go to the appropriate zone based on their input
+    // ADD CODE HERE
+
+  }
+
+  private void enterZone1()
+  {
+    // change image
+    // ADD CODE HERE
+
+    // describe the area/situation to the user. 
+    // Give them options for choices.
+    // ADD CODE HERE
+
+    // Take action or go to another zone based on their choice
+    // ADD CODE HERE
+
+  }
+
+  private void enterZone2()
+  {
+    // change image
+    // ADD CODE HERE
+
+    // describe the area/situation to the user. 
+    // Give them options for choices.
+    // ADD CODE HERE
+
+    // Take action or go to another zone based on their choice
+    // ADD CODE HERE
+    
+  }
+
+  private void enterZone3()
+  {
+    // change image
+    // ADD CODE HERE
+
+    // describe the area/situation to the user. 
+    // Give them options for choices.
+    // ADD CODE HERE
+
+    // Take action or go to another zone based on their choice
+    // ADD CODE HERE
+    
+  }
+
+  private void enterZone4()
+  {
+    // change image
+    // ADD CODE HERE
+
+    // describe the area/situation to the user. 
+    // Give them options for choices.
+    // ADD CODE HERE
+
+    // Take action or go to another zone based on their choice
+    // ADD CODE HERE
+    
+  }
+
+  private void enterZone5()
+  {
+    // change image
+    // ADD CODE HERE
+
+    // describe the area/situation to the user. 
+    // Give them options for choices.
+    // ADD CODE HERE
+
+    // Take action or go to another zone based on their choice
+    // ADD CODE HERE
+    
+  }
+
+  private void enterZone6()
+  {
+    // change image
+    // ADD CODE HERE
+
+    // describe the area/situation to the user. 
+    // Give them options for choices.
+    // ADD CODE HERE
+
+    // Take action or go to another zone based on their choice
+    // ADD CODE HERE
+    
+  }
+
+  private void gameEnd()
+  {
+    // ADD CODE HERE
+
+    inScanner.close();
+  }
+}
